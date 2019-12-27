fa80a065b1434110af7b100f8533bc45c1ba46de
Nikos <printezisn@gmail.com>
2019-12-26 16:10:55 -0800

    Change preview image name.

--- src/templates/post.js
+++ src/templates/post.js
@@ -20,7 +20,7 @@ export default ({ pageContext: { post, siteUrl, pagePath } }) => (
                 property="og:url"
                 content={siteUrl + "post/read/" + post.id + "/"}
             />
-            <meta property="og:image" content={siteUrl + "preview.jpg"} />
+            <meta property="og:image" content={siteUrl + "site.jpg"} />
             <meta property="og:image:type" content="image/jpeg" />
             <meta property="og:image:width" content="400" />
             <meta property="og:image:height" content="400" />
