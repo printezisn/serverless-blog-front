21af8afa1babb85e932b9068a9f80cd1d01572d5
Nikos <printezisn@gmail.com>
2020-01-03 02:07:17 +0200

    Add sitemap and robots file.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,17 +4,23 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
+    siteMetadata: {
+        title: "Edna Blog",
+        siteUrl: process.env.GATSBY_EDNABLOG_SITE_ROOT_URL,
+    },
     plugins: [
         `gatsby-plugin-sass`,
         `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-robots-txt`,
+        `gatsby-plugin-sitemap`,
         {
             resolve: `gatsby-plugin-create-client-paths`,
             options: { prefixes: [`/post/edit/*`, `/post/delete/*`] },
         },
     ],
 }
-
-require("dotenv").config({
-    path: `.env.${process.env.NODE_ENV}`,
-})
